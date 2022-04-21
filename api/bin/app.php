644014706e9c3c50051eb79be09b7d78232db895
donrus <donrusvlad@gmail.com>
2022-04-21 19:26:05 +0300

    initial commit

---
+++ api/bin/app.php
@@ -0,0 +1,50 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\Migrations\Configuration\Configuration;
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\Configuration\Migration\ExistingConfiguration;
+use Doctrine\Migrations\DependencyFactory;
+use Doctrine\Migrations\Metadata\Storage\TableMetadataStorageConfiguration;
+use Doctrine\Migrations\Tools\Console\Helper\ConfigurationHelper;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+if (getenv('SENTRY_DSN')) {
+    Sentry\init(['dsn' => getenv('SENTRY_DSN')]);
+}
+
+/** @var ContainerInterface $container */
+$container = require __DIR__ . '/../config/container.php';
+
+$cli = new Application('Console');
+
+if (getenv('SENTRY_DSN')) {
+    $cli->setCatchExceptions(false);
+}
+
+/**
+ * @var string[] $commands
+ * @psalm-suppress MixedArrayAccess
+ */
+$commands = $container->get('config')['console']['commands'];
+
+/** @var EntityManagerInterface $entityManager */
+$entityManager = $container->get(EntityManagerInterface::class);
+
+$cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
+
+foreach ($commands as $name) {
+    /** @var Command $command */
+    $command = $container->get($name);
+    $cli->add($command);
+}
+
+$cli->run();
